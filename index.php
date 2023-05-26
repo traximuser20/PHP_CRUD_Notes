a35bae20e6b38c595bd3536b8aa096813a560236
TraximUser20 <traximuser20@gmail.com>
2023-05-26 13:17:47 -0700

    PHP configured

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php
+  $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "notes";
+
+        //Create a new connection
+        $conn = mysqli_connect($servername, $username, $password, $database);
+        
+        //Die if connection is not established
+        if (!$conn) {
+          die("Couldn't connect to server: " . mysqli_connect_error());
+        }
+        // else {
+        //   echo "Connecting to server is successful <br>";
+        // }
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -154,8 +172,8 @@
                 class="bg-gray-200 focus:bg-white px-3 block flex-grow flex-shrink overflow-hidden rounded-full"
               />
               <button
-              title="Search"
-              type="submit"
+                title="Search"
+                type="submit"
                 class="flex items-center justify-center relative h-8 w-8 rounded-full"
               >
                 <svg
@@ -302,6 +320,8 @@
               Note Title
             </label>
             <input
+              id="title"
+              name="title"
               required
               class="shadow-xl appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               id="nick"
@@ -317,9 +337,134 @@
             >
               Note Discription
             </label>
+            <div class="border bg-gray-200 overflow-hidden rounded-t-lg shadow-xl">
+              <script
+                src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.js"
+                defer
+              ></script>
+              <style>
+                @import url(https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.min.css);
+
+                .active\:bg-gray-50:active {
+                  --tw-bg-opacity: 1;
+                  background-color: rgba(249, 250, 251, var(--tw-bg-opacity));
+                }
+              </style>
+
+              <script>
+                function app() {
+                  return {
+                    wysiwyg: null,
+                    init: function (el) {
+                      // Get el
+                      this.wysiwyg = el;
+                      // Add CSS
+                      this.wysiwyg.contentDocument.querySelector(
+                        "head"
+                      ).innerHTML += `<style>
+            *, ::after, ::before {box-sizing: border-box;}
+            :root {tab-size: 4;}
+            html {line-height: 1.15;text-size-adjust: 100%;}
+            body {margin: 0px; padding: 1rem 0.5rem;}
+            body {font-family: system-ui, -apple-system, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji";}
+            </style>`;
+                      this.wysiwyg.contentDocument.body.innerHTML += `
+            <h1>Hello World!</h1>
+            <p>Welcome to the pure AlpineJS and Tailwind WYSIWYG.</p>
+            `;
+                      // Make editable
+                      this.wysiwyg.contentDocument.designMode = "on";
+                    },
+                    format: function (cmd, param) {
+                      this.wysiwyg.contentDocument.execCommand(
+                        cmd,
+                        !1,
+                        param || null
+                      );
+                    },
+                  };
+                }
+              </script>
+              <div class="w-full flex border-b text-xl text-gray-600">
+                <button
+                  class="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50"
+                  @click="format('bold')"
+                >
+                  <i class="mdi mdi-format-bold"></i>
+                </button>
+                <button
+                  class="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50"
+                  @click="format('italic')"
+                >
+                  <i class="mdi mdi-format-italic"></i>
+                </button>
+                <button
+                  class="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 mr-1 hover:text-indigo-500 active:bg-gray-50"
+                  @click="format('underline')"
+                >
+                  <i class="mdi mdi-format-underline"></i>
+                </button>
+                <button
+                  class="outline-none focus:outline-none border-l border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50"
+                  @click="format('formatBlock','P')"
+                >
+                  <i class="mdi mdi-format-paragraph"></i>
+                </button>
+                <button
+                  class="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50"
+                  @click="format('formatBlock','H1')"
+                >
+                  <i class="mdi mdi-format-header-1"></i>
+                </button>
+                <button
+                  class="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50"
+                  @click="format('formatBlock','H2')"
+                >
+                  <i class="mdi mdi-format-header-2"></i>
+                </button>
+                <button
+                  class="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 mr-1 hover:text-indigo-500 active:bg-gray-50"
+                  @click="format('formatBlock','H3')"
+                >
+                  <i class="mdi mdi-format-header-3"></i>
+                </button>
+                <button
+                  class="outline-none focus:outline-none border-l border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50"
+                  @click="format('insertUnorderedList')"
+                >
+                  <i class="mdi mdi-format-list-bulleted"></i>
+                </button>
+                <button
+                  class="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 mr-1 hover:text-indigo-500 active:bg-gray-50"
+                  @click="format('insertOrderedList')"
+                >
+                  <i class="mdi mdi-format-list-numbered"></i>
+                </button>
+                <button
+                  class="outline-none focus:outline-none border-l border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50"
+                  @click="format('justifyLeft')"
+                >
+                  <i class="mdi mdi-format-align-left"></i>
+                </button>
+                <button
+                  class="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50"
+                  @click="format('justifyCenter')"
+                >
+                  <i class="mdi mdi-format-align-center"></i>
+                </button>
+                <button
+                  class="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50"
+                  @click="format('justifyRight')"
+                >
+                  <i class="mdi mdi-format-align-right"></i>
+                </button>
+              </div>
+            </div>
             <textarea
+              id="desc"
+              name="desc"
               required
-              class="shadow-xl resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+              class="shadow-xl resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded-b-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-teal-600 h-48 resize-none"
               id="message"
             ></textarea>
           </div>
@@ -336,6 +481,19 @@
           <div class="md:w-2/3"></div>
         </div>
       </form>
+    </div>
+    <!-- component -->
+
+    <div class="h-0.5 w-full bg-teal-400 drop-shadow-2xl"></div>
+    <div class=" ">
+      <?php
+          $sql = "SELECT * FROM `notes`";
+          $result = mysqli_query($conn, $sql);
+          while($row = mysqli_fetch_assoc($result)) {
+            echo $row['num']. ". <strong>Title: </strong> ". $row['title']. " <br><strong>Description: </strong>". $row['desc'];
+            echo "<br>";
+          }
+        ?>
     </div>
   </body>
 </html>
